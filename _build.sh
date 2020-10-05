2bc62da8d163a411b88ba6a11891005b09c0e3b9
Michael <michu@neophob.com>
2020-10-05 20:42:19 +0200

    update build script

--- _build.sh
+++ _build.sh
@@ -3,5 +3,8 @@ FILES=./dump*.json
 for f in $FILES
 do
   echo "Processing $f file..."
-  node lib/visualise/index.js "$f"
+  node lib/visualise/index.js "$f" a3dpi1200&
+  node lib/visualise/index.js "$f" a3dpi1200_10s&
+  node lib/visualise/index.js "$f" a3dpi1200_short&
+  wait
 done
